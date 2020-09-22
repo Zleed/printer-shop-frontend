4c474e4b6fe5971bf47f79cd5dc15cc2a2871e5f
Zleed <tamasfloch95@gmail.com>
2020-09-22 22:58:54 +0100

    App.js => App.jsx, added missing <navBar/> into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import HomePage from "./component/page/HomePage";
 import NavBar from "./component/navbar/NavBar";
 
 function App() {
     return (
-        <div className="App">
-            <Router>
+        <BrowserRouter>
+            <Switch>
                 <NavBar/>
-                <Switch>
+                <div className="App">
                     <Route path="/" exact component={HomePage}/>
-                </Switch>
-            </Router>
-        </div>
+                </div>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
